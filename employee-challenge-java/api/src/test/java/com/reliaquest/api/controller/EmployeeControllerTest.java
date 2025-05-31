4cd4dcd32d6a776e32bea62bdf5efbbcc057e6ee
Anunay Sinha <anunay9a@gmail.com>
2025-05-31 10:00:39 +0530

    test: add test case

--- employee-challenge-java/api/src/test/java/com/reliaquest/api/controller/EmployeeControllerTest.java
+++ employee-challenge-java/api/src/test/java/com/reliaquest/api/controller/EmployeeControllerTest.java
@@ -1,5 +1,6 @@
 package com.reliaquest.api.controller;
 
+import com.reliaquest.api.dto.EmployeeDTO;
 import com.reliaquest.api.exception.ClientBadRequestException;
 import com.reliaquest.api.exception.ResourceNotFoundException;
 import com.reliaquest.api.service.EmployeeService;
@@ -10,8 +11,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -29,16 +32,34 @@ public class EmployeeControllerTest {
     class GetEmployeeByIdTests {
 
         @Test
-        @DisplayName("should throw ClientBadRequestException when ID is null or blank")
-        void shouldThrowClientBadRequestException_whenIdIsNullOrBlank() {
+        @DisplayName("should return Employee when employee ID is valid")
+        void shouldReturnEmployee_whenEmployeeIdIsValid() {
+            // Arrange
+            String validId = "123e4567-e89b-12d3-a456-425514174000";
+            EmployeeDTO mockValidEmployee = new EmployeeDTO();
+            when(employeeService.getEmployeeById(validId))
+                    .thenReturn(mockValidEmployee);
+
+            // Act
+            ResponseEntity<EmployeeDTO> response = employeeController.getEmployeeById(validId);
+
+            // Assert
+            assertEquals(200, response.getStatusCode().value());
+            assertEquals(mockValidEmployee, response.getBody());
+            verify(employeeService).getEmployeeById(validId);
+        }
+
+        @Test
+        @DisplayName("should throw ClientBadRequestException when employee ID is null or blank")
+        void shouldThrowClientBadRequestException_whenEmployeeIdIsNullOrBlank() {
             // Arrange
             String nullId = null;
             String blankId = "";
 
             // Act & Assert
             assertThrows(
-                ClientBadRequestException.class,
-                () -> employeeController.getEmployeeById(nullId)
+                    ClientBadRequestException.class,
+                    () -> employeeController.getEmployeeById(nullId)
             );
 
             assertThrows(
@@ -49,21 +70,20 @@ public class EmployeeControllerTest {
 
         @Test
         @DisplayName("should throw ResourceNotFoundException when employee ID does not exist")
-        void shouldThrowResourceNotFoundException_whenGivenIdDoesNotExist() {
+        void shouldThrowResourceNotFoundException_whenEmployeeIdDoesNotExist() {
             // Arrange
             String notExistingId = "123e4567-e89b-12d3-a456-426614174000";
 
             // Act
             when(employeeService.getEmployeeById(notExistingId))
-                .thenThrow(new ResourceNotFoundException("Employee not found"));
+                    .thenThrow(new ResourceNotFoundException("Employee not found"));
 
             // Assert
             assertThrows(
-                ResourceNotFoundException.class,
-                () -> employeeController.getEmployeeById(notExistingId)
+                    ResourceNotFoundException.class,
+                    () -> employeeController.getEmployeeById(notExistingId)
             );
         }
-
     }
 
 }
