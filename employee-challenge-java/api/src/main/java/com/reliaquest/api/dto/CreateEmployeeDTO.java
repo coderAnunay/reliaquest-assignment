bccf44ad699e5e33e5a387a2f8c9da79a917e7cf
Anunay Sinha <anunay9a@gmail.com>
2025-05-31 10:46:12 +0530

    refactor: spotless apply

--- employee-challenge-java/api/src/main/java/com/reliaquest/api/dto/CreateEmployeeDTO.java
+++ employee-challenge-java/api/src/main/java/com/reliaquest/api/dto/CreateEmployeeDTO.java
@@ -9,12 +9,10 @@ public class CreateEmployeeDTO {
     @NotBlank(message = "Name must not be blank")
     private String name;
 
-    @NotNull(message = "Salary is required")
-    @Min(value = 1, message = "Salary must be greater than zero")
+    @NotNull(message = "Salary is required") @Min(value = 1, message = "Salary must be greater than zero")
     private Integer salary;
 
-    @NotNull(message = "Age is required")
-    @Min(value = 16, message = "Minimum allowed age is 16")
+    @NotNull(message = "Age is required") @Min(value = 16, message = "Minimum allowed age is 16")
     @Max(value = 75, message = "Maximum allowed age is 75")
     private Integer age;
 
